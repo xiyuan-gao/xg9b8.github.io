6e76ec5574cd8d4f044a5272e8861520004929a2
xiyuan-gao <80716870+xiyuan-gao@users.noreply.github.com>
2023-04-01 23:45:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-window.onbeforeunload = function() {
-  window.scrollTo(0, 0);
+window.onload = function() {
+  document.querySelector("#home").style.display = "block";
+  // hide other sections
+  var sections = document.querySelectorAll("main > section");
+  for (var i = 0; i < sections.length; i++) {
+    if (sections[i].id !== "home") {
+      sections[i].style.display = "none";
+    }
+  }
 };
+
 function showSection(sectionId) {
     const sections = document.querySelectorAll("main section");
     for (const section of sections) {
